7726349cbfc41896b5779226b5b696c7ade0ceb5
GabrielPonti <gabrielponti991@gmail.com>
2025-06-03 19:20:36 -0400

    testando arquivos estaticos 15

--- app/settings.py
+++ app/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = '/opt/render/project/src/staticfiles'
 
  
 # para usar o css e favicon 
